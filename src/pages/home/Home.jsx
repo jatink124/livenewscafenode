6c93a1a4d0ba63e24f35754157b209b25d1c37af
jatink124 <kaushaljatin48@gmail.com>
2021-09-28 22:07:56 +0530

    initial commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("/posts" + search);
+      const res = await axios.get("https://sleepy-fjord-84120.herokuapp.com/api/posts" + search);
       setPosts(res.data);
     };
     fetchPosts();
