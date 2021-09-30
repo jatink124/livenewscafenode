38a0fefbe010f63a2b41cedab0ccab7dbe58d490
jatink124 <kaushaljatin48@gmail.com>
2021-09-30 18:16:56 +0530

    write jsx

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -23,12 +23,12 @@ export default function Write() {
       data.append("file", file);
       newPost.photo = filename;
       try {
-        await axios.post("https://sleepy-fjord-84120.herokuapp.com/api/upload", data);
+        await axios.post("/upload", data);
       } catch (err) {}
     }
     try {
-      const res = await axios.post("https://sleepy-fjord-84120.herokuapp.com/api/posts", newPost);
-      window.location.replace("https://sleepy-fjord-84120.herokuapp.com/api/post/" + res.data._id);
+      const res = await axios.post("/posts", newPost);
+      window.location.replace("/post/" + res.data._id);
     } catch (err) {}
   };
   return (
