dafb8f6aa3875c953a5a0bf85cfcf70acd348d16
jatink124 <kaushaljatin48@gmail.com>
2021-10-01 22:29:49 +0530

    kughu

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -23,11 +23,11 @@ export default function Write() {
       data.append("file", file);
       newPost.photo = filename;
       try {
-        await axios.post("https://secure-falls-74774.herokuapp.com/https://sleepy-fjord-84120.herokuapp.com/api/upload", data);
+        await axios.post("https://sleepy-fjord-84120.herokuapp.com/api/upload", data);
       } catch (err) {}
     }
     try {
-      const res = await axios.post("https://secure-falls-74774.herokuapp.com/https://sleepy-fjord-84120.herokuapp.com/api/posts", newPost);
+      const res = await axios.post("https://sleepy-fjord-84120.herokuapp.com/api/posts", newPost);
       // window.location.replace("/post/" + res.data._id);
       // window.location.replace("/");
     } catch (err) {}
