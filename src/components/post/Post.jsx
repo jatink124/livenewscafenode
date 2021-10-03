8711a86dbf2ec92da253500c4ef58d97d2f1440d
jatink124 <kaushaljatin48@gmail.com>
2021-10-03 18:20:57 +0530

    guyh

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -2,7 +2,7 @@ import "./post.css";
 import { Link } from "react-router-dom";
 
 export default function Post({ post }) {
-  const PF = "https://sleepy-fjord-84120.herokuapp.com/images/";
+  const PF = "https://sleepy-fjord-84120.herokuapp.com/api/images/";
   return (
     <div className="post">
       {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
